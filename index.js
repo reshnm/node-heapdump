0beb5977b7a18c151f62b7ca5f1fadc4fcf59b5a
Rene Schuenemann <rene.schuenemann@gmail.com>
2020-12-17 12:36:27 +0100

    Added files

---
+++ index.js
@@ -0,0 +1,29 @@
+const heapdump = require('heapdump');
+const archiver = require('archiver');
+const fs = require('fs');
+const http = require('http');
+
+function writeHeapSnapshot() {
+    const snapshotName = Date.now() + '.heapsnapshot';
+    heapdump.writeSnapshot(snapshotName, function(err, snapshotFile) {
+        const output = fs.createWriteStream(snapshotName + '.zip');
+        const archive = archiver('zip', {
+            zlib: { level: 9 }
+        });
+
+        archive.pipe(output);
+        archive.append(fs.createReadStream(snapshotFile), { name: snapshotFile });
+        archive.finalize();
+
+        output.on('close', function() {
+            fs.unlinkSync(snapshotFile);
+            console.log(`Heap snapshot archive has been written to ${snapshotName}.zip (${archive.pointer()} total bytes)`);
+        });
+    });
+}
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    writeHeapSnapshot();
+    res.end('Hello');
+}).listen(process.env.PORT || 3000);
